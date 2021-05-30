5e0e784b5bbd36a5b557cf0fc80f2cd81a79b5ae
DESKTOP-46BEDUC\garbi <garbiozyldz@gmail.com>
2021-05-30 14:29:53 +0300

    Unit tests

--- src/test/kotlin/com/modanisa/application/ToDoServiceApplicationTests.kt
+++ src/test/kotlin/com/modanisa/application/ToDoServiceApplicationTests.kt
@@ -3,10 +3,10 @@ package com.modanisa.application
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-//@SpringBootTest
+@SpringBootTest
 class ToDoServiceApplicationTests {
 
-//	@Test
+	@Test
 	fun contextLoads() {
 	}
 
